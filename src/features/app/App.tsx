929176ec13c7301f14ebfdf327c6a58ef19d9c87
Martin Couso <martin.couso@equinox.com>
2022-10-30 14:10:55 -0300

    chore: clean up Home

--- src/features/app/App.tsx
+++ src/features/app/App.tsx
@@ -8,32 +8,8 @@
  * @format
  */
 
-import React, {type PropsWithChildren, useEffect} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  withTiming,
-  withSpring,
-  withRepeat,
-} from 'react-native-reanimated';
 import {NavigationContainer} from '@react-navigation/native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {createDrawerNavigator} from '@react-navigation/drawer';
@@ -41,36 +17,11 @@ import Home from '../home/home';
 import PanGestureHandlerBasics from '../pan-gesture-handler-basics/PanGestureHandlerBasics';
 import InterpolateScrollView from '../interpolate-scroll-view/InterpolateScrollView';
 import ColorInterpolationBasic from '../color-interpolation/ColoInterpolation';
+import SkiaHoloCards from '../skia-holo-cards/SkiaHoloCards';
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    flex: 1,
-  };
-
-  const progress = useSharedValue(1);
-  const scale = useSharedValue(2);
-
-  const reanimatedStyle = useAnimatedStyle(() => {
-    return {
-      opacity: progress.value,
-      transform: [
-        {scale: scale.value},
-        {rotate: `${progress.value * 2 * Math.PI}rad`},
-      ],
-      borderRadius: (progress.value * 100) / 2,
-    };
-  }, []);
-
-  useEffect(() => {
-    progress.value = withRepeat(withTiming(0.5, {duration: 2000}), -1, true);
-    scale.value = withRepeat(withSpring(1, {}), -1, true);
-  }, [progress, scale]);
-
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <NavigationContainer>
@@ -100,29 +51,23 @@ const App = () => {
               },
             }}
           />
+          <Drawer.Screen
+            name="Holo Cards Skia"
+            component={SkiaHoloCards}
+            options={{
+              headerStyle: {
+                backgroundColor: 'black',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+          />
         </Drawer.Navigator>
       </NavigationContainer>
     </GestureHandlerRootView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -8,24 +8,10 @@
  * @format
  */
 
-import React, {type PropsWithChildren, useEffect} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, StatusBar, useColorScheme, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
 import Animated, {
   useSharedValue,
@@ -88,24 +74,5 @@ const Home = () => {
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default Home;
